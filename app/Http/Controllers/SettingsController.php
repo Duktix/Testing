80a3eaa9977d1254b2c80721931571f93e33b49a
jyoti <jbora201@gmail.com>
2018-02-11 12:07:33 +0530

    Updated 11-2-2018

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -68,7 +68,6 @@ class SettingsController extends Controller
     {
 		$process = new Process('cd '.base_path().';git reset --hard origin/master');
 		$process->run();
-		dd($process->getOutput());
 		return redirect()->back();
     }
 	public function saveSettings(Request $requests)

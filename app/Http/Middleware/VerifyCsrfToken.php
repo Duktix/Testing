3e25924f0a030c6654bac163614394021f29b759
jyoti <jbora201@gmail.com>
2018-02-16 08:35:44 +0530

    Updated

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        "settings/resetdb"
+        "settings/resetdb",
+		"settings/update"
     ];
 }
